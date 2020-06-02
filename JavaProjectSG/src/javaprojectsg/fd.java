99dadb54aa0cfbedbcc84786d52bf19bfbffb541
Kavya <Kavya@DESKTOP-59QE7FO>
2020-06-02 12:03:37 +0530

    feedback

--- JavaProjectSG/src/javaprojectsg/fd.java
+++ JavaProjectSG/src/javaprojectsg/fd.java
@@ -37,11 +37,16 @@ public class fd extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         rt = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        un = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
+        pn = new javax.swing.JTextField();
+        submit = new javax.swing.JButton();
 
         jLabel1.setText("jLabel1");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setBackground(new java.awt.Color(102, 255, 255));
+        setBackground(new java.awt.Color(51, 153, 255));
 
         jLabel2.setText("FEEDBACK");
 
@@ -51,30 +56,71 @@ public class fd extends javax.swing.JFrame {
             }
         });
 
+        jLabel3.setText("USERNAME");
+
+        un.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                unActionPerformed(evt);
+            }
+        });
+
+        jLabel4.setText("PRODUCT NAME");
+
+        pn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                pnActionPerformed(evt);
+            }
+        });
+
+        submit.setText("SUBMIT");
+        submit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                submitActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(78, 78, 78)
-                .addComponent(jLabel2)
-                .addGap(77, 77, 77)
-                .addComponent(rt, javax.swing.GroupLayout.PREFERRED_SIZE, 159, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(152, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel3)
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel4))
+                .addGap(36, 36, 36)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(pn, javax.swing.GroupLayout.PREFERRED_SIZE, 117, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(un, javax.swing.GroupLayout.PREFERRED_SIZE, 191, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addComponent(rt, javax.swing.GroupLayout.PREFERRED_SIZE, 159, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(submit)))
+                .addContainerGap(234, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(77, 77, 77)
+                .addGap(56, 56, 56)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
-                    .addComponent(rt, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(170, Short.MAX_VALUE))
+                    .addComponent(un, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3))
+                .addGap(50, 50, 50)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(pn, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 73, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(rt, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2))
+                .addGap(70, 70, 70)
+                .addComponent(submit)
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
+/*
     private void rtActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_rtActionPerformed
         Connection con;
         
@@ -97,6 +143,77 @@ public class fd extends javax.swing.JFrame {
             JOptionPane.showMessageDialog(null,"not connected");
         }        // TODO add your handling code here:
     }//GEN-LAST:event_rtActionPerformed
+
+    private void unActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_unActionPerformed
+       Connection con;
+        
+        try {
+            String dburl= "jdbc:mysql://localhost:3306/shoppingguide?zeroDateTimeBehavior=convertToNull";
+	    String user="root";
+	    String pass="abcd.1234";
+	    con=DriverManager.getConnection(dburl,user,pass);
+            String query="insert into feedbacks(uname) values(?)";
+            PreparedStatement pst= con.prepareStatement(query);
+            pst.setString(1, un.getText());
+            pst.executeUpdate();
+             JOptionPane.showMessageDialog(null,"successfully inserted");
+            
+            
+            
+            
+        } catch (SQLException ex) {
+            
+            JOptionPane.showMessageDialog(null,"not connected");
+        }        // TODO add your handling code here:
+    }//GEN-LAST:event_unActionPerformed
+
+    private void pnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_pnActionPerformed
+        Connection con;
+        
+        try {
+            String dburl= "jdbc:mysql://localhost:3306/shoppingguide?zeroDateTimeBehavior=convertToNull";
+	    String user="root";
+	    String pass="abcd.1234";
+	    con=DriverManager.getConnection(dburl,user,pass);
+            String query="insert into feedbacks(pname) values(?)";
+            PreparedStatement pst= con.prepareStatement(query);
+            pst.setString(1, pn.getText());
+            pst.executeUpdate();
+             JOptionPane.showMessageDialog(null,"successfully inserted");
+            
+            
+            
+            
+        } catch (SQLException ex) {
+            
+            JOptionPane.showMessageDialog(null,"not connected");
+        }       
+    }//GEN-LAST:event_pnActionPerformed
+*/
+    private void submitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_submitActionPerformed
+         Connection con;
+        
+        try {
+            String dburl= "jdbc:mysql://localhost:3306/shoppingguide?zeroDateTimeBehavior=convertToNull";
+	    String user="root";
+	    String pass="abcd.1234";
+	    con=DriverManager.getConnection(dburl,user,pass);
+            String query="insert into feedbacks(uname,pname,fdbk) values(?,?,?)";
+            PreparedStatement pst= con.prepareStatement(query);
+            pst.setString(1, un.getText());
+            pst.setString(2, pn.getText());
+            pst.setString(3, rt.getText());
+            pst.executeUpdate();
+             JOptionPane.showMessageDialog(null,"successfully inserted");
+            
+            
+            
+            
+        } catch (SQLException ex) {
+            
+            JOptionPane.showMessageDialog(null,"not connected");
+        }        // TODO add your handling code here:
+    }//GEN-LAST:event_submitActionPerformed
 
     /**
      * @param args the command line arguments
@@ -136,6 +253,11 @@ public class fd extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JTextField pn;
     private javax.swing.JTextField rt;
+    private javax.swing.JButton submit;
+    private javax.swing.JTextField un;
     // End of variables declaration//GEN-END:variables
 }
